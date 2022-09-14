3f3b35cdb1cac506ef688f9f055a8b6dd4f794eb
Victor Hugo <victorhugoos@live.com>
2022-09-14 01:29:14 -0300

    feat: created flatlist for the gamecards

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
@@ -21,8 +21,17 @@ export function Home() {
         subtitle="Find the game you want to play..."
       />
 
-      <GameCard 
-        data={GAMES[0]}
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={( { item } ) => (
+          <GameCard 
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
 
     </View>
