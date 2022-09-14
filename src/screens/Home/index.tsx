28168db82eb5cd48cdb7ef961954ef1edd08a032
Victor Hugo <victorhugoos@live.com>
2022-09-14 01:12:18 -0300

    feat: created gamecard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
+
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
@@ -17,6 +19,10 @@ export function Home() {
       <Heading 
         title="Find your duo"
         subtitle="Find the game you want to play..."
+      />
+
+      <GameCard 
+        data={GAMES[0]}
       />
 
     </View>
