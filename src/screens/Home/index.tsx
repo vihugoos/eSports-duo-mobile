84293f12b2acf8f287a1321171d3542891735cd4
Victor Hugo <victorhugoos@live.com>
2022-09-14 00:39:59 -0300

    feat: created logo and heading component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading 
+        title="Find your duo"
+        subtitle="Find the game you want to play..."
+      />
 
     </View>
   );
