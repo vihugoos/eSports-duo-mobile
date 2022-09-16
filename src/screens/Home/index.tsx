89d200b2eb5e74bc6d610d6b0490b60059083e2c
Victor Hugo <victorhugoos@live.com>
2022-09-16 18:02:15 -0300

    feat: changed to safeareaview

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { GameCard, GameCardProps} from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
@@ -18,7 +19,7 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image 
         source={logoImg}
         style={styles.logo}
@@ -42,6 +43,6 @@ export function Home() {
         contentContainerStyle={styles.contentList}
       />
 
-    </View>
+    </SafeAreaView>
   );
 }
