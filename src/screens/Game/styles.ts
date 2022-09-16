bb03921a0dc420e9114a8c1284b25f5a74f8bc50
Victor Hugo <victorhugoos@live.com>
2022-09-16 20:10:56 -0300

    feat: created game component

--- src/screens/Game/styles.ts
+++ src/screens/Game/styles.ts
@@ -1,5 +1,30 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  cover: {
+    width: 311,
+    height: 160,
+    borderRadius: 8,
+    marginTop: 32
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: 'space-between'
+  },
+  logo: {
+    width: 85,
+    height: 53
+  },
+  right: {
+    width: 20,
+    height: 20  
+  }
 });
